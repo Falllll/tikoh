e0ff9407ce67a913e42ab9b926d7f28726929361
Falllll <faldiharido@gmail.com>
2022-08-08 06:21:42 +0700

    extracting the authenticated layout

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +15,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory()->create(['email' => 'faldi@gmail.com', 'password' => bcrypt('secret')]);
     }
 }
