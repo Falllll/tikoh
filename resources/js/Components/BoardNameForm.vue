ad1eb79153c08ab1f2b3afd4e5b7b3a3db2622cb
Falllll <faldiharido@gmail.com>
2022-08-08 06:51:31 +0700

    InertiaJS inline editable board name but better

--- resources/js/Components/BoardNameForm.vue
+++ resources/js/Components/BoardNameForm.vue
@@ -9,24 +9,13 @@ const props = defineProps({
 const form = useForm({
   name: props.board.name
 });
-const heading = ref();
 const input = ref();
 const isEditing = ref(false);
-
-function resize() {
-  input.value.style.width = `${heading.value.offsetWidth + 2}px`;
-}
-
-watch(() => form.name, async () => {
-  await nextTick();
-  resize();
-});
 
 async function edit() {
   isEditing.value = true;
   await nextTick();
   input.value.select();
-  resize();
 }
 
 function onSubmit() {
@@ -37,11 +26,10 @@ function onSubmit() {
 }
 </script>
 <template>
-  <div class="flex flex-col items-start max-w-full">
+  <div class="flex flex-col items-start max-w-full relative">
     <h1
-      ref="heading"
-      :class="[isEditing ? 'absolute -left-[1000rem]': '']"
-      class="hover:bg-white/20 whitespace-pre-wrap break-all border border-transparent rounded-md cursor-pointer px-3 py-1.5 text-2xl text-white font-bold"
+      :class="[isEditing ? 'invisible': '']"
+      class="hover:bg-white/20 whitespace-pre w-full overflow-hidden text-ellipsis border border-transparent rounded-md cursor-pointer px-3 py-1.5 text-xl text-white font-bold"
       @click="edit()"
     >{{ form.name ? form.name : ' ' }}</h1>
     <form
@@ -53,7 +41,7 @@ function onSubmit() {
       <input
         ref="input"
         v-model="form.name"
-        class="text-2xl max-w-full font-bold placeholder-gray-400 px-3 py-1.5 rounded-md focus:ring-2 focus:ring-blue-900"
+        class="absolute inset-0 text-xl max-w-full font-bold placeholder-gray-400 px-3 py-1.5 rounded-md focus:ring-2 focus:ring-blue-900"
         placeholder="Board name"
         type="text"
       >
