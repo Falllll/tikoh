161980a5b48edfa45c700e3ae765ea466deca4fc
Falllll <faldiharido@gmail.com>
2022-08-08 00:59:26 +0700

    pop up

--- resources/js/Pages/Board.vue
+++ resources/js/Pages/Board.vue
@@ -1,15 +1,38 @@
 <script setup>
 import {DotsHorizontalIcon, PencilIcon, PlusIcon} from "@heroicons/vue/solid";
+import {Menu, MenuButton, MenuItem, MenuItems} from '@headlessui/vue';
+
 </script>
 <template>
   <div class="flex flex-col h-screen bg-blue-600">
     <header class="shrink-0 flex justify-between bg-white px-4 py-3">
       <a class="text-2xl font-black tracking-tight" href="/">kanboard</a>
-      <nav>
+      <nav class="flex items-center">
         <a class="text-sm font-medium px-3 py-2 rounded-md hover:bg-gray-100" href="#">My boards</a>
-        <button class="ml-3">
-          <img class="h-9 w-9 inline rounded-full" src="https://pbs.twimg.com/profile_images/1333896976602193922/MtWztkxt_400x400.jpg" alt="">
-        </button>
+
+        <Menu as="div" class="ml-3 relative z-10">
+          <MenuButton class="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-400 rounded-full">
+            <img class="h-9 w-9 inline rounded-full" src="https://pbs.twimg.com/profile_images/1333896976602193922/MtWztkxt_400x400.jpg" alt="">
+          </MenuButton>
+
+          <transition
+            enter-active-class="transition transform duration-100 ease-out"
+            enter-from-class="opacity-0 scale-90"
+            enter-to-class="opacity-100 scale-100"
+            leave-active-class="transition transform duration-100 ease-in"
+            leave-from-class="opacity-100 scale-100"
+            leave-to-class="opacity-0 scale-90"
+          >
+            <MenuItems class="origin-top-right mt-2 focus:outline-none absolute right-0 bg-white overflow-hidden rounded-md shadow-lg border w-48">
+              <MenuItem v-slot="{active}">
+                <a href="#" :class="{'bg-gray-100': active}" class="block px-4 py-2 text-sm text-gray-700">My Profile</a>
+              </MenuItem>
+              <MenuItem v-slot="{active}">
+                <a href="#" :class="{'bg-gray-100': active}" class="block px-4 py-2 text-sm text-gray-700">Log out</a>
+              </MenuItem>
+            </MenuItems>
+          </transition>
+        </Menu>
       </nav>
     </header>
 
@@ -31,9 +54,29 @@ import {DotsHorizontalIcon, PencilIcon, PlusIcon} from "@heroicons/vue/solid";
               class="w-72 bg-gray-200 max-h-full flex flex-col rounded-md">
               <div class="flex items-center justify-between px-3 py-2">
                 <h3 class="text-sm font-semibold text-gray-700">Backlog</h3>
-                <button class="hover:bg-gray-300 w-8 h-8 rounded-md grid place-content-center">
-                  <DotsHorizontalIcon class="w-5 h-5"/>
-                </button>
+                <Menu as="div" class="relative z-10">
+                  <MenuButton class="hover:bg-gray-300 w-8 h-8 rounded-md grid place-content-center">
+                    <DotsHorizontalIcon class="w-5 h-5"/>
+                  </MenuButton>
+
+                  <transition
+                    enter-active-class="transition transform duration-100 ease-out"
+                    enter-from-class="opacity-0 scale-90"
+                    enter-to-class="opacity-100 scale-100"
+                    leave-active-class="transition transform duration-100 ease-in"
+                    leave-from-class="opacity-100 scale-100"
+                    leave-to-class="opacity-0 scale-90"
+                  >
+                    <MenuItems class="origin-top-left mt-2 focus:outline-none absolute left-0 bg-white overflow-hidden rounded-md shadow-lg border w-40">
+                      <MenuItem v-slot="{active}">
+                        <a href="#" :class="{'bg-gray-100': active}" class="block px-4 py-2 text-sm text-gray-700">Add card</a>
+                      </MenuItem>
+                      <MenuItem v-slot="{active}">
+                        <a href="#" :class="{'bg-gray-100': active}" class="block px-4 py-2 text-sm text-red-600">Delete list</a>
+                      </MenuItem>
+                    </MenuItems>
+                  </transition>
+                </Menu>
               </div>
               <div class="pb-3 flex flex-col overflow-hidden">
                 <div class="px-3 flex-1 overflow-y-auto">
@@ -41,12 +84,6 @@ import {DotsHorizontalIcon, PencilIcon, PlusIcon} from "@heroicons/vue/solid";
                     <li
                       v-for="item in Array.from({length: 7})"
                       class="group relative bg-white p-3 shadow rounded-md border-b border-gray-300 hover:bg-gray-50">
-                      <a href="#" class="text-sm">card item</a>
-                      <button class="hidden absolute top-1 right-1 w-8 h-8 bg-gray-50 group-hover:grid place-content-center rounded-md text-gray-600 hover:text-black hover:bg-gray-200">
-                        <PencilIcon class="w-5 h-5"/>
-                      </button>
-                    </li>
-                    <li class="group relative bg-white p-3 shadow rounded-md border-b border-gray-300 hover:bg-gray-50">
                       <a href="#" class="text-sm">card item</a>
                       <button class="hidden absolute top-1 right-1 w-8 h-8 bg-gray-50 group-hover:grid place-content-center rounded-md text-gray-600 hover:text-black hover:bg-gray-200">
                         <PencilIcon class="w-5 h-5"/>
